9e9a04739806f42494d152750050bf2521b6744a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-04-26 22:17:31 +0900

    feat: app init

--- apps/with-auth/src/pages/protected.tsx
+++
@@ -1,9 +0,0 @@
-import React, { VFC } from 'react';
-
-import { AuthLayout } from '../auth/AuthContext';
-
-const Protected: VFC = () => {
-  return <AuthLayout>Protected Page</AuthLayout>;
-};
-
-export default Protected;
